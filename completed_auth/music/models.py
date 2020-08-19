d472f731c8266fc5aebd70e6902e5aaa5241874b
Sam <sam.eskandar00@hotmail.com>
2020-08-18 22:44:06 -0400

    add image field

--- completed_auth/music/models.py
+++ completed_auth/music/models.py
@@ -5,6 +5,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Music(models.Model):
     name = models.CharField(max_length=100)
+    image = models.TextField(
+        max_length=10000,
+        default="https://yt3.ggpht.com/iMT9MVrt6qxAfTxeKUX17ESdppsDntW2eA9YvnONcPqxlbdt9SkVhaIRsAtE0PFqRiLA-arexQ=s900-c-k-c0xffffffff-no-rj-mo",
+        null=True
+    )
     album = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
     rating = models.FloatField(default=1.0)
